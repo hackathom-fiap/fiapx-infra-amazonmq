03cd57aa4be9588441e9389924d0a6609caa0131
hmouraf <humbertomoura@gmail.com>
2026-02-21 13:36:39 -0300

    new amazonmq

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_secretsmanager_secret" "mq_password" {
   name = "${var.broker_name}-password"
 }
 
+
 resource "aws_secretsmanager_secret_version" "mq_password" {
   secret_id     = aws_secretsmanager_secret.mq_password.id
   secret_string = random_password.mq_password.result
